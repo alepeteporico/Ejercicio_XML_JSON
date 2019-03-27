2c6c8e59cdd093488f32ce3b39df9f91f0a84a23
alepeteporico <tojandro@gmail.com>
2019-03-27 17:17:25 +0100

    ejercicio 1 y 2 de xml terminado

---
+++ programa_xml.py
@@ -0,0 +1,36 @@
+from lxml import etree
+doc=etree.parse('radares.xml')
+
+while True:
+    print("================================================================================")
+    print("1.Mostrar el nombre de las provincias de las que tenemos información sobre radares.")
+    print("2.Mostrar la cantidad de radares de los que tenemos información.")
+    print("3.Pedir por teclado una provincia y mostrar el nombre de las carreteras que tiene y la cantidad de radares.")
+    print("4.Pedir por teclado una carretera, muestra las provincias por la que pasa y sus respectivos radares.")
+    print("5.Pedir por teclado una carretera, cuenta los radares que tiene y muestra las coordenadas de los radares.(Se puede obtener la URL de OpenStraeetMap para ver donde está el radar).")
+    print("6.Salir")
+    print("================================================================================")
+    elec=int(input("Elige una opción: "))
+    print("")
+
+    if elec==6:
+        break
+
+    elif elec==1:
+        lista=doc.xpath("/RAIZ/PROVINCIA/NOMBRE/text()")
+    
+        for provincia in lista:
+            print("-",provincia)
+        print("------------------------------")
+        intro=input("Pulsa enter para continuar")
+        print("")
+    
+    elif elec==2:
+        lista=doc.xpath("/RAIZ/PROVINCIA/CARRETERA/RADAR")
+        print(len(lista),"radares hay")
+        print("------------------------------")
+        intro=input("Pulsa enter para continuar")
+        print("")
+    
+    elif elec==3:
+        
